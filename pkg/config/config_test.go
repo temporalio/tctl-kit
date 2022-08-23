c15c5b702eba777ead3dcd8f0debb20d8961c1d2
feedmeapples <aksenov.ro@outlook.com>
2022-08-22 23:41:44 -0400

    Refactor config and fix it for Windows

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-package config
+package config_test
 
 import (
 	"errors"
@@ -30,6 +30,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/temporalio/tctl-kit/pkg/config"
 )
 
 const (
@@ -56,78 +57,78 @@ func TestNewConfigCreatesFile(t *testing.T) {
 	}
 }
 
-func TestConfigDefaults(t *testing.T) {
-	cfg := initConfig(t)
-	defer removeConfig(t)
-
-	env, err := cfg.Get(KeyCurrentEnvironment)
-	assert.NoError(t, err)
-	assert.Equal(t, env, "local")
+type envprop struct {
+	env, key, value string
 }
 
-func TestConfigSetKey(t *testing.T) {
-	tests := map[string]struct {
+func TestAlias(t *testing.T) {
+	testcases := map[string]struct {
+		input  string
+		expect map[string]string
+	}{
+		"reads alias by name": {
+			input: `aliases:
+    wj: workflow list --output json
+    wt: workflow list --output table`,
+			expect: map[string]string{
+				"wj":           "workflow list --output json",
+				"wt":           "workflow list --output table",
+				"doesnt-exist": "",
+				"":             "",
+			},
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			writeConfig(t, tc.input)
+			cfg := initConfig(t)
+			defer removeConfig(t)
+
+			for aliasName, vExpected := range tc.expect {
+				vActual := cfg.Alias(aliasName)
+				assert.Equal(t, vActual, vExpected)
+			}
+		})
+	}
+}
+
+func TestSetAlias(t *testing.T) {
+	testcases := map[string]struct {
 		input    map[string]string
 		expected string
 		err      bool
 	}{
-		"throw on empty key": {
+		"throws on empty key": {
 			input: map[string]string{
 				"": "value",
 			},
 			err: true,
 		},
-		"throw on invalid key": {
+		"throws on invalid key": {
 			input: map[string]string{
-				"1key": "value",
-				"-key": "value",
 				"key!": "value",
+				"-key": "value",
+				"k.ey": "value",
 			},
 			err: true,
 		},
-		"accepts empty value": {
+		"sets on proper key and value ": {
 			input: map[string]string{
-				"key": "",
+				"wt": "workflow list --output table",
+				"wj": "workflow list --output json",
 			},
-			expected: "env:\n  local: {}\nkey: \"\"\n",
-			err:      false,
-		},
-		"valid key and value": {
-			input: map[string]string{
-				"valid-key":                  "value 1",
-				"valid-key2":                 "value 2",
-				"valid-key3.xxx-yyy_zzz.ooo": "value 3",
-			},
-			expected: `valid-key: value 1
-valid-key2: value 2
-valid-key3:
-  xxx-yyy_zzz:
-    ooo: value 3
-`,
-			err: false,
-		},
-		"merge keys": {
-			input: map[string]string{
-				"env.local.key1":  "value-local-1",
-				"env.local.key2":  "value-local-2",
-				"env.remote.key1": "value-remote-1",
-			},
-			expected: `  local:
-    key1: value-local-1
-    key2: value-local-2
-  remote:
-    key1: value-remote-1`,
-			err: false,
+			expected: "aliases:\n    wj: workflow list --output json\n    wt: workflow list --output table",
 		},
 	}
 
-	for name, tc := range tests {
+	for name, tc := range testcases {
 		t.Run(name, func(t *testing.T) {
 			cfg := initConfig(t)
 			defer removeConfig(t)
 
 			for key, value := range tc.input {
-				err := cfg.Set(key, value)
+				err := cfg.SetAlias(key, value)
 				if tc.err {
 					assert.Error(t, err)
 				} else {
@@ -142,8 +143,76 @@ valid-key3:
 	}
 }
 
-func TestConfigGetEnv(t *testing.T) {
-	tests := map[string]struct {
+func TestCurrentEnv(t *testing.T) {
+	testcases := map[string]struct {
+		input  string
+		expect map[string]string
+	}{
+		"reads alias by name": {
+			input: `aliases:
+    wj: workflow list --output json
+    wt: workflow list --output table`,
+			expect: map[string]string{
+				"wj":           "workflow list --output json",
+				"wt":           "workflow list --output table",
+				"doesnt-exist": "",
+				"":             "",
+			},
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			writeConfig(t, tc.input)
+			cfg := initConfig(t)
+			defer removeConfig(t)
+
+			for aliasName, vExpected := range tc.expect {
+				vActual := cfg.Alias(aliasName)
+				assert.Equal(t, vActual, vExpected)
+			}
+		})
+	}
+}
+
+func TestSetCurrentEnv(t *testing.T) {
+	testcases := map[string]struct {
+		input    string
+		expected string
+		err      bool
+	}{
+		"throws on empty value": {
+			input: "",
+			err:   true,
+		},
+		"throws on invalid value": {
+			input: "wrong-env-name!",
+			err:   true,
+		},
+		"sets on proper value ": {
+			input:    "dev",
+			expected: "current-env: dev",
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			cfg := initConfig(t)
+			defer removeConfig(t)
+
+			err := cfg.SetCurrentEnv(tc.input)
+			if tc.err {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Contains(t, readConfig(t), tc.expected)
+			}
+		})
+	}
+}
+
+func TestEnv(t *testing.T) {
+	testcases := map[string]struct {
 		input  string
 		expect map[string]map[string]string
 	}{
@@ -166,15 +235,14 @@ func TestConfigGetEnv(t *testing.T) {
 		},
 	}
 
-	for name, tc := range tests {
+	for name, tc := range testcases {
 		t.Run(name, func(t *testing.T) {
 			writeConfig(t, tc.input)
 			cfg := initConfig(t)
 			defer removeConfig(t)
 
 			for envName, envProps := range tc.expect {
-				envActual, err := cfg.GetEnv(envName)
-				assert.NoError(t, err)
+				envActual := cfg.Env(envName)
 				for key, vExpected := range envProps {
 					vActual := envActual[key]
 					assert.Equal(t, vActual, vExpected)
@@ -184,8 +252,168 @@ func TestConfigGetEnv(t *testing.T) {
 	}
 }
 
-func initConfig(t *testing.T) *Config {
-	cfg, err := NewConfig(appName, cfgFile)
+func TestRemoveEnv(t *testing.T) {
+	testcases := map[string]struct {
+		inputCfg    string
+		inputRemove string
+		expected    string
+		err         bool
+	}{
+		"throws on empty name": {
+			inputRemove: "",
+			err:         true,
+		},
+		"throws on invalid name": {
+			inputRemove: "wrong-env-name!",
+			err:         true,
+		},
+		"throws on removing current env": {
+			inputCfg:    "current-env: dev",
+			inputRemove: "dev",
+			err:         true,
+		},
+		"removes env on proper name": {
+			inputCfg: `
+current-env: remote
+env:
+  local:
+    key1: value-local-1
+    key2: value-local-2
+  remote:
+    key1: value-remote-1`,
+			inputRemove: "local",
+			expected: `
+current-env: remote
+env:
+    remote:
+        key1: value-remote-1`,
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			writeConfig(t, tc.inputCfg)
+			cfg := initConfig(t)
+			defer removeConfig(t)
+
+			err := cfg.RemoveEnv(tc.inputRemove)
+			if tc.err {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Contains(t, readConfig(t), tc.expected)
+			}
+		})
+	}
+}
+
+func TestEnvProperty(t *testing.T) {
+	testcases := map[string]struct {
+		input  string
+		expect map[string]map[string]string
+	}{
+		"reads env property by key": {
+			input: `env:
+  local:
+    key1: value-local-1
+    key2: value-local-2
+  remote:
+    key1: value-remote-1`,
+			expect: map[string]map[string]string{
+				"local": {
+					"key1": "value-local-1",
+					"key2": "value-local-2",
+				},
+				"remote": {
+					"key1": "value-remote-1",
+				},
+			},
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			writeConfig(t, tc.input)
+			cfg := initConfig(t)
+			defer removeConfig(t)
+
+			for envName, envProps := range tc.expect {
+				for key, vExpected := range envProps {
+					vActual := cfg.EnvProperty(envName, key)
+					assert.Equal(t, vActual, vExpected)
+				}
+			}
+		})
+	}
+
+}
+
+func TestSetEnvProperty(t *testing.T) {
+	testcases := map[string]struct {
+		input    []envprop
+		expected string
+		err      bool
+	}{
+		"throws on empty key": {
+			input: []envprop{{"", "key", "value"}, {"local", "", "value"}},
+			err:   true,
+		},
+		"throws on invalid key": {
+			input: []envprop{
+				{"1local", "key", "value"},
+				{"local", "1key", "value"},
+				{"-local", "key", "value"},
+				{"local", "-key", "value"},
+				{"local!", "key", "value"},
+				{"local", "key!", "value"},
+				{"lo.cal", "key", "value"},
+				{"local", "ke.y", "value"},
+			},
+			err: true,
+		},
+		"accepts empty value": {
+			input:    []envprop{{"local", "key", ""}},
+			expected: "env:\n    local:\n        key: \"\"\n",
+			err:      false,
+		},
+		"merges env properties": {
+			input: []envprop{
+				{"local", "key1", "value-local-1"},
+				{"local", "key2", "value-local-2"},
+				{"remote", "key3", "value-remote-3"},
+			},
+			expected: `local:
+        key1: value-local-1
+        key2: value-local-2
+    remote:
+        key3: value-remote-3`,
+			err: false,
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			cfg := initConfig(t)
+			defer removeConfig(t)
+
+			for _, envprop := range tc.input {
+				err := cfg.SetEnvProperty(envprop.env, envprop.key, envprop.value)
+				if tc.err {
+					assert.Error(t, err)
+				} else {
+					assert.NoError(t, err)
+				}
+			}
+
+			if !tc.err {
+				assert.Contains(t, readConfig(t), tc.expected)
+			}
+		})
+	}
+}
+
+func initConfig(t *testing.T) *config.Config {
+	cfg, err := config.NewConfig(appName, cfgFile)
 	assert.NoError(t, err)
 
 	return cfg
